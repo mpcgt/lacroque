bb92f4521706158afc4e862cf218e71dcc319674
Max <max.pcgt@gmail.com>
2024-04-25 17:11:41 +0200

    Updates files

--- LaCroque/navigation.js
+++ LaCroque/navigation.js
@@ -3,11 +3,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 import { View, Text } from 'react-native'
 import React from 'react'
+import HomeScreen from './screens/HomeScreen';
+import BoulangerieScreen from './screens/BoulangerieScreen';
 
-export default function navigation() {
+export default function Navigation() {
   return (
-    <View>
-      <Text>navigation</Text>
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Boulangerie" component={BoulangerieScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   )
 }
